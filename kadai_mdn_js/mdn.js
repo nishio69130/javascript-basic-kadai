68f4f02940c1ded5e58af21f2f4d2ac8e7e92118
nozomu-nishitanin <nishio69130@gmail.com>
2024-10-13 20:45:02 +0900

    2024/10/13 20:44

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,7 @@
+const myDate = new Date();
+
+const year = myDate.getFullYear();
+const month = myDate.getMonth();
+const date =  myDate.getDate();
+
+console.log(year + '年' + month + '月' + date + '日') ;
