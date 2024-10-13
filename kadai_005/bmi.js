eacdad7e78f67a4a010eff59512f565fdc4028e1
nozomu-nishitanin <nishio69130@gmail.com>
2024-10-13 18:57:54 +0900

    2024/10/13 18:57

--- kadai_005/bmi.js
+++ kadai_005/bmi.js
@@ -1,3 +1,5 @@
 const weight = 68; //体重（kg）
 const height = 1.7; //身長(m)
-console.log(weight / (height*height));  //BMI 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+let bmi;
+bmi = weight / (height*height);  //BMI 計算式：[体重(kg)]÷[身長(m)×身長(m)]
+console.log(bmi);
